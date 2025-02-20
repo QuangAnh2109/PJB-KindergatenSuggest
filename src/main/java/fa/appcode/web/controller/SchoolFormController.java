ad516d4cff4d62f43ee78e7112a5391698dfafb5
anhdq43 <anhdq43@fpt.com>
2025-02-20 10:23:45 +0700

    [module-03][anhdq43] rename add-new-school-form.html to school-manager-detail.html and add button: delete, edit, reject, approve, publish, unpublish

--- src/main/java/fa/appcode/web/controller/SchoolFormController.java
+++ src/main/java/fa/appcode/web/controller/SchoolFormController.java
@@ -1,5 +1,6 @@
 package fa.appcode.web.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class SchoolFormController {
     @GetMapping("/admin/school-form")
-    public String home() {
-        return "admin_side/add-new-school-form";
+    public String home(HttpServletRequest res) {
+        res.setAttribute("schoolIntroduction", "<p>Hello World!</p>\n<p>Some initial <strong>bold</strong> text</p>\n<p><br /></p>");
+        return "school-form";
     }
 }
