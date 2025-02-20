40c97d4cf221311671ad2e989808c771513f2615
AnhNT510 <anhnt510@fpt.com>
2025-02-20 10:01:40 +0700

    [Module-01][AnhNT510] Authentication and authority and Login Ui

---
+++ src/main/java/fa/appcode/common/constant/Constant.java
@@ -0,0 +1,7 @@
+package fa.appcode.common.constant;
+
+import fa.appcode.web.controller.CustomAuthenticationSuccessHandler;
+
+public class Constant {
+    public final String email_Regex="^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+}
