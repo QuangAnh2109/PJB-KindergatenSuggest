2b7e4ae3c70c517f8a1b9c644151f141425d6b13
PhuDK <PhuDK@fpt.com>
2025-02-21 13:49:59 +0700

    [Module-04][PhuDK]: Hien thi List-parent va detail-parent

--- src/main/java/fa/appcode/web/controller/ParentController.java
+++ src/main/java/fa/appcode/web/controller/ParentController.java
@@ -1,26 +1,70 @@
 package fa.appcode.web.controller;
 
+import fa.appcode.common.vo.EnrolledSchoolVo;
+import fa.appcode.common.vo.ParentVo;
+import fa.appcode.config.GlobalConfig;
+import fa.appcode.entities.AccountInfo;
+import fa.appcode.repositories.AccountRepository;
+import fa.appcode.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @Controller
 @RequestMapping("/")
 public class ParentController {
 
-    @GetMapping("/admin/parent-list/parent-details")
-    public String parent_details() {
+    @Autowired
+    private AccountService accountService;
+    @Autowired
+    private GlobalConfig globalConfig;
+
+    @Autowired
+    private AccountRepository accountRepository;
+    @GetMapping("/admin/parent-list/parent-details/{id}")
+    public String parent_details(@RequestParam(name = "currentPage"
+            ,defaultValue = "0") int currentPage,@PathVariable("id") int id,Model model) {
+        Pageable pageable = PageRequest.of(currentPage, 10,
+                Sort.by("id").ascending());
+        ParentVo accountInfo = accountService.findParentById(id);
+        Page<EnrolledSchoolVo> listParentEnroll = accountService.findEnrolledSchoolBy(id,pageable);
+        List<EnrolledSchoolVo> enrolledSchools = listParentEnroll.getContent();
+        model.addAttribute("enrolledSchools", enrolledSchools);
+        model.addAttribute("accountInfo", accountInfo);
+        model.addAttribute("currentPage", currentPage);
+        model.addAttribute("numberPage", listParentEnroll.getTotalPages());
         return "admin_side/parent-details";
     }
 
+//    @GetMapping("/admin/parent-list")
+//
+//    public String parent_list(Model model) {
+//        model.addAttribute("id",1);
+//        return "admin_side/parent-list";
+//    }
+
     @GetMapping("/admin/parent-list")
-
-    public String parent_list(Model model) {
-        model.addAttribute("id",1);
-        return "admin_side/parent-list";
+    public String test(@RequestParam(name = "currentPage"
+              ,defaultValue = "0") int currentPage, Model model) {
+        Pageable pageable = PageRequest.of(currentPage, 5,
+                  Sort.by("id").ascending());
+        Page<ParentVo> list = accountService.findAllParent(pageable);
+        List<ParentVo> accounts = list.getContent();
+//        List<AccountInfo> roles = accountService.findAllRoles();
+        model.addAttribute("accounts", accounts);
+//        model.addAttribute("roles", roles)
+        model.addAttribute("currentPage", currentPage);
+        model.addAttribute("numberPage", list.getTotalPages());
+        return"admin_side/parent-list";
     }
-
 //    @GetMapping("/test")
 //
 //    public AccountVo getAccount(@PathVariable Integer id) {
