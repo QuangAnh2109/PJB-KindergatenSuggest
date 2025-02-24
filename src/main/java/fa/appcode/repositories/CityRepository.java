cd52bcfc1e724ec73dd0081a9e80a43e5f1a9a5a
Haivd7 <haivd7@fpt.com>
2025-02-24 11:12:29 +0700

    [Module-06][Haivd7] : Add City repository

--- src/main/java/fa/appcode/repositories/CityRepository.java
+++ src/main/java/fa/appcode/repositories/CityRepository.java
@@ -2,6 +2,11 @@ package fa.appcode.repositories;
 
 import fa.appcode.entities.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CityRepository extends JpaRepository<City, Integer> {
+    @Query("SELECT c.id,c.cityName FROM City c")
+    public List<City> getAllCities();
 }
--- src/main/java/fa/appcode/web/controller/UserHomeController.java
+++ src/main/java/fa/appcode/web/controller/UserHomeController.java
@@ -1,16 +1,20 @@
 package fa.appcode.web.controller;
 
 import fa.appcode.config.GlobalConfig;
+import fa.appcode.entities.City;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserHomeController {
 
     @GetMapping("/home")
     public String parentHome(Model model) {
-
+        List<City> listCity = new ArrayList<>();
         return "user_side/index";
     }
     @GetMapping("/search")
