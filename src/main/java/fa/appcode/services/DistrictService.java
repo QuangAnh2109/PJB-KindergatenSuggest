c7297150f3d696e419c9fe74c3e08643f752f059
anhdq43 <anhdq43@fpt.com>
2025-02-25 11:05:15 +0700

    [module-03][anhdq43] add findAllByCityIdAndDeleteFlg method to DistrictRepository, DistrictService, DistrictServiceImpl

--- src/main/java/fa/appcode/services/DistrictService.java
+++ src/main/java/fa/appcode/services/DistrictService.java
@@ -1,7 +1,9 @@
 package fa.appcode.services;
 
-import fa.appcode.repositories.DistrictRepository;
+import fa.appcode.vo.DistrictVo;
+
+import java.util.List;
 
 public interface DistrictService {
-
+    List<DistrictVo> findAllByCityIdAndDeleteFlg(Integer cityId, Boolean deleteFlg);
 }
--- src/main/java/fa/appcode/services/impl/DistrictServiceImpl.java
+++ src/main/java/fa/appcode/services/impl/DistrictServiceImpl.java
@@ -1,6 +1,20 @@
 package fa.appcode.services.impl;
 
+import fa.appcode.repositories.DistrictRepository;
 import fa.appcode.services.DistrictService;
+import fa.appcode.vo.DistrictVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class DistrictServiceImpl implements DistrictService {
+    @Autowired
+    private DistrictRepository districtRepository;
+
+    @Override
+    public List<DistrictVo> findAllByCityIdAndDeleteFlg(Integer cityId, Boolean deleteFlg) {
+        return districtRepository.findAllByCityIdAndDeleteFlg(cityId, deleteFlg);
+    }
 }
