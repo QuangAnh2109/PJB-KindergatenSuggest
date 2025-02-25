e077361dd36e4fed1575602eed5b4cbcdb8ba630
anhdq43 <anhdq43@fpt.com>
2025-02-25 11:07:00 +0700

    [module-03][anhdq43] add findAllByDeleteFlg method to CityRepository, CityService, CityServiceImpl

--- src/main/java/fa/appcode/services/impl/CityServiceImpl.java
+++ src/main/java/fa/appcode/services/impl/CityServiceImpl.java
@@ -1,6 +1,20 @@
 package fa.appcode.services.impl;
 
+import fa.appcode.repositories.CityRepository;
 import fa.appcode.services.CityService;
+import fa.appcode.vo.CityVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class CityServiceImpl implements CityService {
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Override
+    public List<CityVo> findAllByDeleteFlg(Boolean deleteFlg){
+        return cityRepository.findAllByDeleteFlg(deleteFlg);
+    }
 }
