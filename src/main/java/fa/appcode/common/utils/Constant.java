0e2a64d91432b902f967eac2a0e1e96eca59ec97
PhuDK <PhuDK@fpt.com>
2025-02-25 11:19:54 +0700

    [Module-04][PhuDK]: Enroll Parent to school

---
+++ src/main/java/fa/appcode/common/utils/Constant.java
@@ -0,0 +1,6 @@
+package fa.appcode.common.utils;
+
+public class Constant {
+    public static final String SCHOOL_AND_ENROLL_INIT_PAGE = "0";
+    public static final String KEY_WORD_DEFAULT = "";
+}
--- src/main/java/fa/appcode/web/controller/ParentController.java
+++ src/main/java/fa/appcode/web/controller/ParentController.java
@@ -1,5 +1,6 @@
 package fa.appcode.web.controller;
 
+import fa.appcode.common.utils.Constant;
 import fa.appcode.common.vo.EnrolledSchoolVo;
 import fa.appcode.common.vo.ParentVo;
 import fa.appcode.config.GlobalConfig;
@@ -20,13 +21,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
 import java.time.Instant;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 
 
@@ -44,9 +43,10 @@ public class ParentController {
     @Autowired
     private SchoolInfoService schoolInfoService;
 
+
     @GetMapping({"admin/parent-list", "school-owner/parent-list","admin/parent-list/parent-details", "school-owner/parent-list/parent-details"})
     public String parentList(@RequestParam(name = "currentPage"
-              ,defaultValue = "0") int currentPage, @RequestParam(defaultValue = "") String search, Model model, @RequestHeader(value = "X-Requested-With", required = false) String requestedWith, Principal principal) {
+              ,defaultValue = Constant.SCHOOL_AND_ENROLL_INIT_PAGE) int currentPage, @RequestParam(defaultValue = Constant.KEY_WORD_DEFAULT) String search, Model model, @RequestHeader(value = "X-Requested-With", required = false) String requestedWith, Principal principal) {
 
         /*
          * Setup pageable
@@ -73,12 +73,13 @@ public class ParentController {
         if ("XMLHttpRequest".equals(requestedWith)) {
             return "admin_side/parent-list :: main-content";
         }
-        return"admin_side/parent-list";
+        return "admin_side/parent-list";
     }
+
 
     @GetMapping("admin/parent-list/parent-details/{id}")
     public String parentDetailsAdmin(@RequestParam(name = "currentPage"
-            ,defaultValue = "0") int currentPage, @PathVariable("id") int id, Model model,Principal principal,  @RequestHeader(value = "X-Requested-With", required = false) String requestedWith) {
+            ,defaultValue = Constant.SCHOOL_AND_ENROLL_INIT_PAGE) int currentPage, @PathVariable("id") int id, Model model,Principal principal,  @RequestHeader(value = "X-Requested-With", required = false) String requestedWith) {
 
         /*
          * Setup pageable
@@ -115,9 +116,10 @@ public class ParentController {
         return "admin_side/parent-details";
     }
 
+
     @GetMapping("school-owner/parent-list/parent-details/{id}")
     public String parentDetailsSchoolOwner(@RequestParam(name = "currentPage"
-            ,defaultValue = "0") int currentPage, @PathVariable("id") int id, Model model, Principal principal,  @RequestHeader(value = "X-Requested-With", required = false) String requestedWith) {
+            ,defaultValue = Constant.SCHOOL_AND_ENROLL_INIT_PAGE) int currentPage, @PathVariable("id") int id, Model model, Principal principal,  @RequestHeader(value = "X-Requested-With", required = false) String requestedWith) {
         /*
          * Setup pageable
          */
@@ -152,55 +154,47 @@ public class ParentController {
         }
         return "admin_side/parent-details";
     }
+
+
     @Transactional
-    @PostMapping("school-owner/parent-list/parent-details/{id}")
-    public String enrollParentToSchool(@RequestParam(name = "currentPage"
-            ,defaultValue = "0") int currentPage, @PathVariable("id") int id, Model model, Principal principal,@RequestParam("school") int schoolId) {
+    @PostMapping({"school-owner/parent-list/parent-details/{id}", "admin/parent-list/parent-details/{id}"})
+    public String enrollParentToSchool(@PathVariable("id") int id, Model model, @RequestParam("school") int schoolId, RedirectAttributes redirectAttributes, Principal principal) {
         /*
          * Enroll Parent to School
          */
-        EnrollSchool schoolEnroll = new EnrollSchool();
-        schoolEnroll.setAccount(accountService.getAccountInfoById(id));
-        schoolEnroll.setSchool(schoolInfoService.getSchoolInfoById(schoolId));
-        schoolEnroll.setEnrollDate(LocalDate.now());
-        schoolEnroll.setStatus(true);
-        schoolEnroll.setCreateId("SCHOOL_OWNER");
-        schoolEnroll.setCreateTime(Instant.now());
-        schoolEnroll.setUpdateId("SCHOOL_OWNER");
-        schoolEnroll.setUpdateTime(Instant.now());
-        schoolEnroll.setDeleteFlg(false);
-        enrollSchoolService.enrollSchool(schoolEnroll);
-        /*
-         * Page setting
-         */
-
-        Pageable pageable = PageRequest.of(currentPage, 5,
-                Sort.by("id").ascending());
-        /*
-         * Get page from Service
-         */
-        ParentVo accountInfo = accountService.findParentById(id);
-        Page<EnrolledSchoolVo> listParentEnroll = accountService.findParentEnrolledSchoolByParentIdAndSchoolOwner(id, principal.getName(), pageable);
-        List<EnrolledSchoolVo> enrolledSchools = listParentEnroll.getContent();
-
-        /*
-         * Get Schools from Service
-         */
-        List<SchoolInfo> schoolInfoList = schoolInfoService.findSchoolInfoByAccountId(principal.getName());
         String role = accountService.findByEmail(principal.getName()).getAccountRole().replaceAll(" ","-").toLowerCase();
-        /*
-         * Put data into Model
-         */
-        model.addAttribute("role", role);
-        model.addAttribute("schoolInfoList", schoolInfoList);
-        model.addAttribute("enrolledSchools", enrolledSchools);
-        model.addAttribute("accountInfo", accountInfo);
-        model.addAttribute("currentPage", currentPage);
-        model.addAttribute("numberPage", listParentEnroll.getTotalPages());
+        try {
+            // Enroll Parent to School
+            EnrollSchool schoolEnroll = new EnrollSchool();
+            schoolEnroll.setAccount(accountService.getAccountInfoById(id));
+            schoolEnroll.setSchool(schoolInfoService.getSchoolInfoById(schoolId));
+            schoolEnroll.setEnrollDate(LocalDate.now());
+            schoolEnroll.setStatus(true);
+            schoolEnroll.setCreateTime(Instant.now());
+            schoolEnroll.setUpdateTime(Instant.now());
+            schoolEnroll.setDeleteFlg(false);
+            if(role.equals("school-owner")) {
+                schoolEnroll.setCreateId("SCHOOL_OWNER");
+                schoolEnroll.setUpdateId("SCHOOL_OWNER");
+            }
+            if(role.equals("admin")) {
+                schoolEnroll.setCreateId("ADMIN");
+                schoolEnroll.setUpdateId("ADMIN");
+            }
+            enrollSchoolService.enrollSchool(schoolEnroll);
+
+            // Success message
+            redirectAttributes.addFlashAttribute("message", "Parent enrolled successfully to " + schoolEnroll.getSchool().getSchoolName());
+            redirectAttributes.addFlashAttribute("alertType", "success");
+        } catch (Exception e) {
+            // Error message
+            redirectAttributes.addFlashAttribute("message", "Failed to enroll parent!");
+            redirectAttributes.addFlashAttribute("alertType", "danger");
+        }
         /*
          * Return view name
          */
-        return "admin_side/parent-details";
+        return "redirect:/"+role+"/parent-list/parent-details/" + id;
     }
 
 }
