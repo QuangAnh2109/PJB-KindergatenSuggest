ecae63b5bba02a1ff83bb5df5798fec4a8d64be6
anhdq43 <anhdq43@fpt.com>
2025-02-25 11:02:15 +0700

    [module-03][anhdq43] create Vo entity: MasterData, City, District, Ward

---
+++ src/main/java/fa/appcode/vo/DistrictVo.java
@@ -0,0 +1,13 @@
+package fa.appcode.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class DistrictVo {
+    private Integer id;
+    private String districtName;
+}
---
+++ src/main/java/fa/appcode/vo/WardVo.java
@@ -0,0 +1,14 @@
+package fa.appcode.vo;
+
+import fa.appcode.entities.District;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class WardVo {
+    private Integer id;
+    private String wardName;
+}
