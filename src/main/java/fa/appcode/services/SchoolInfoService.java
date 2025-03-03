9dc083a009b0a60657fac1c2c4ace0367d27f028
Quang Anh <anhdq2109@gmail.com>
2025-03-03 20:51:59 +0700

    [module-03][anhdq43] update SchoolInfoService

--- src/main/java/fa/appcode/services/SchoolInfoService.java
+++ src/main/java/fa/appcode/services/SchoolInfoService.java
@@ -1,13 +1,28 @@
 package fa.appcode.services;
 
+import fa.appcode.common.vo.SchoolInfoVo;
 import fa.appcode.entities.SchoolInfo;
 
 import java.util.List;
 
 public interface SchoolInfoService {
     List<SchoolInfo> findSchoolInfoListByAccountEmail(String email);
+
     List<SchoolInfo> findAllSchoolPublished();
+
     SchoolInfo getSchoolInfoById(int id);
+
     List<Integer> getAllSchoolIdsByAccountEmail(String email);
+
     List<Integer> getAllSchoolIdsForUnenrollParentByAccountEmail(String email);
+
+    SchoolInfoVo findSchoolInfoVoByIdAndAccountIdNoDelete(int id, int accountId);
+
+    SchoolInfoVo findSchoolInfoVoByIdNoDelete(int id);
+
+    SchoolInfo findSchoolInfoByIdAndAccountIdNoDelete(int id, int accountId);
+
+    SchoolInfo findSchoolInfoByIdNoDelete(int id);
+
+    SchoolInfo save(SchoolInfo schoolInfo);
 }
