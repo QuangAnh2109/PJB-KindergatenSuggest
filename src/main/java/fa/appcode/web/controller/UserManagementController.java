e5d874ab86a264f30743a94bda65c0c62fc3ef00
tuanpa79 <tuanpa79@fpt.com>
2025-03-07 14:59:25 +0700

    [module-02][tuanpa79]: Move edit and add user to RestController

--- src/main/java/fa/appcode/web/controller/UserManagementController.java
+++ src/main/java/fa/appcode/web/controller/UserManagementController.java
@@ -8,6 +8,7 @@ import fa.appcode.config.GlobalConfig;
 import fa.appcode.entities.MasterDatum;
 import fa.appcode.services.AccountService;
 import fa.appcode.services.EmailService;
+import fa.appcode.services.MasterDatumService;
 import fa.appcode.services.impl.MasterDatumServiceImpl;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -27,17 +28,17 @@ import java.util.UUID;
 
 
 @Controller
-@RequiredArgsConstructor
 @RequestMapping("/admin/")
 public class UserManagementController {
 
-    private final GlobalConfig globalConfig;
+    @Autowired
+    private GlobalConfig globalConfig;
 
-    private final AccountService accountService;
+    @Autowired
+    private AccountService accountService;
 
-    private final MasterDatumServiceImpl masterDatumService;
-
-    private final EmailService emailService;
+    @Autowired
+    private MasterDatumService masterDatumService;
 
     /**
      * Pagination, search, show list of movies
@@ -67,111 +68,42 @@ public class UserManagementController {
     }
 
     /**
-     * show Add User screen
+     * Edit or Add user account
      *
+     * @param id
+     * @param principal
      * @param model
      * @return
      */
-    @GetMapping("add-user")
-    public String showAddUserPage(Model model, Principal principal) {
+
+
+    @GetMapping({"add-user", "edit-user/{id}"})
+    public String showUserForm(@PathVariable(name = "id", required = false) Integer id, Model model, Principal principal) {
         String role = accountService.getAccountInfo(principal).getRoleId() == 1 ? "Admin" : "School owner";
-        model.addAttribute("role", role);
-        model.addAttribute("user", new AccountVo()); // Gửi một user rỗng để form hiển thị đúng
+
+        AccountVo user;
+        if (id != null) {
+            try {
+                user = accountService.getAccountById(id);
+            } catch (EntityNotFoundException e) {
+                model.addAttribute("error", "User not found.");
+                return Constant.VIEW_ACCOUNT_PAGE;
+            }
+        } else {
+            user = new AccountVo(); // Nếu là thêm mới, tạo đối tượng rỗng
+        }
 
         List<MasterDatum> roles = masterDatumService.getListByTypeName("ROLE");
         List<MasterDatum> status = masterDatumService.getListByTypeName("ACCOUNT STATUS");
+
+        model.addAttribute("role", role);
+        model.addAttribute("user", user);
         model.addAttribute("roles", roles);
         model.addAttribute("status", status);
 
-        return "admin_side/EditUser"; // Sử dụng trang EditUser.html
-    }
-
-    /**
-     * Adds a new user account
-     *
-     * @param accountVo
-     * @param redirectAttributes
-     * @return
-     */
-
-    @PostMapping("add-user")
-    public String addUser(@ModelAttribute("user") AccountVo accountVo, Principal principal, RedirectAttributes redirectAttributes) {
-        try {
-
-            // Auto-generated password
-            String randomPassword = UUID.randomUUID().toString();
-
-            accountVo.setPassword(randomPassword);
-            accountVo.setConfirmPassword(randomPassword);
-
-            accountService.addUserFromAdmin(accountVo);  // Add
-
-
-            //Send mail contains information to login
-            emailService.sendEmailToMany(SendMailInfo.builder().toMail(List.of(accountVo.getEmail()))
-                    .ccMail(List.of())
-                    .mailId(2)
-                    .detail(Map.of(Placeholder.USER_NAME, accountVo.getEmail(),
-                            Placeholder.EMAIL, accountVo.getEmail(),
-                            Placeholder.PASSWORD, randomPassword,
-                            Placeholder.OWNER_ACCOUNT, accountService.getAccountInfo(principal).getFullName()))
-                    .build());
-
-            redirectAttributes.addFlashAttribute("message", "User added successfully.");
-            return "redirect:/admin/add-user";
-        } catch (Exception e) {
-            redirectAttributes.addFlashAttribute("error", "An error occurred: " + e.getMessage());
-            return "redirect:/admin/add-user";
-        }
+        return Constant.VIEW_ACCOUNT_PAGE;
     }
 
 
-    /**
-     * show Edit user account screen
-     *
-     * @param id
-     * @param model
-     * @return
-     */
-    @GetMapping("edit-user/{id}")
-    public String showEditUserPage(@PathVariable("id") Integer id, Model model, Principal principal) {
-        try {
-            AccountVo user = accountService.getAccountById(id);
-            String role = accountService.getAccountInfo(principal).getRoleId() == 1 ? "Admin" : "School owner";
-
-            List<MasterDatum> roles = masterDatumService.getListByTypeName("ROLE");
-            List<MasterDatum> status = masterDatumService.getListByTypeName("ACCOUNT STATUS");
-            // Lấy danh sách roles từ service
-            model.addAttribute("role", role);
-            model.addAttribute("user", user);
-            model.addAttribute("roles", roles);
-            model.addAttribute("status", status);
-        } catch (EntityNotFoundException e) {
-            model.addAttribute("error", "User not found.");
-        }
-        return "admin_side/EditUser";
-    }
-
-    /**
-     * Update user account
-     *
-     * @param accountVo
-     * @param redirectAttributes
-     * @return
-     */
-    @PostMapping("edit-user/{id}")
-    public String updateUser(@ModelAttribute("user") AccountVo accountVo, RedirectAttributes redirectAttributes) {
-        try {
-            accountService.updateUser(accountVo);
-            redirectAttributes.addFlashAttribute("message", "Change has been successfully updated.");
-            return "redirect:/admin/edit-user/" + accountVo.getId();
-        } catch (EntityNotFoundException e) {
-            redirectAttributes.addFlashAttribute("error", "User not found.");
-            return "redirect:/admin/edit-user/" + accountVo.getId();
-        } catch (Exception e) {
-            redirectAttributes.addFlashAttribute("error", "An error occurred: " + e.getMessage());
-            return "redirect:/admin/edit-user/" + accountVo.getId();
-        }
-    }
 
 }
--- src/main/resources/static/admin_side/js/EditUser.js
+++ src/main/resources/static/admin_side/js/EditUser.js
@@ -1,32 +1,24 @@
-setTimeout(function () {
-    $(".alert").remove();
-}, 3000); // Hide alert after 3 seconds
+// setTimeout(function () {
+//     $(".alert").remove();
+// }, 3000); // Hide alert after 3 seconds
 
-$("body").on("click", "button#save-user", function(event) {
-    $(".text-danger").html(""); // Clear old error messages
+$("body").on("submit", "#userForm", function (event) {
+    event.preventDefault();
 
+    $(".error-message").html("");
+    $(".text-danger").html("");
     var count = 0;
     var phoneRegex = /^[0-9]{10,15}$/;
     var today = new Date().toISOString().split("T")[0];
-    var emailRegex = /^[a-z][a-z0-9]*@gmail.com/;
 
-    // Validate Full Name
     if ($("#fullName").val().trim() === '') {
         $("#errorFullName").html('Please enter full name');
         count++;
     }
-
-    // Validate Email
-    var email = $("#email").val().trim();
-    if (email === '') {
+    if ($("#email").val().trim() === '') {
         $("#errorEmail").html('Please enter email');
         count++;
-    } else if (!emailRegex.test(email)) {
-        $("#errorEmail").html('Invalid email format');
-        count++;
     }
-
-    // Validate DOB
     var dob = $("#dob").val();
     if (dob.trim() === '') {
         $("#errorDob").html('Please enter date of birth');
@@ -35,8 +27,6 @@ $("body").on("click", "button#save-user", function(event) {
         $("#errorDob").html('Date of birth must be in the past');
         count++;
     }
-
-    // Validate Phone
     var phone = $("#phone").val().trim();
     if (phone === '') {
         $("#errorPhone").html('Please enter phone number');
@@ -45,98 +35,58 @@ $("body").on("click", "button#save-user", function(event) {
         $("#errorPhone").html('Invalid phone number format');
         count++;
     }
-
-    // Validate Role
     if ($('#role').val() === '') {
         $("#errorRole").html('Please select a role');
         count++;
     }
-
-    // Validate Status
     if ($('#status').val() === '') {
         $("#errorStatus").html('Please select a status');
         count++;
     }
 
-    if (count > 0) {
-        event.preventDefault();
-    }
 
+    if (count === 0) {
+        var user = {
+            id: $("#userID").val() || null,
+            fullName: $("#fullName").val().trim(),
+            email: $("#email").val().trim(),
+            dob: $("#dob").val().trim(),
+            phone: $("#phone").val().trim(),
+            role: $('#role').val().trim(),
+            status: $("#status").val().trim()
+
+
+        };
+        $.ajax({
+            url: "/admin/api/save-user",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(user),
+            success: function (response) {
+                alert(response.message);
+            },
+            error: function (xhr) {
+                var errors = xhr.responseJSON;
+                if (errors) {
+                    $("#errorFullName").html(errors.fullName);
+                    $("#errorEmail").html(errors.email);
+                    $("#errorDob").html(errors.dob);
+                    $("#errorPhone").html(errors.phone);
+                    $("#errorRole").html(errors.role);
+                    $("#errorStatus").html(errors.status);
+                }
+            }
+        });
+    }
 });
 
-// $("body").on("click", "button#save-user", function() {
-//     $("#errorFullName").html('');
-//     $("#errorEmail").html('');
-//     $("#errorDob").html('');
-//     $("#errorPhone").html('');
-//     $("#errorRole").html('');
-//     $("#errorStatus").html('');
-//
-//     var count = 0;
-//     var phoneRegex = /^[0-9]{10,15}$/; // Thay bằng regex từ fa.appcode.common.Constant.phone_regex
-//     var today = new Date().toISOString().split("T")[0]; // Lấy ngày hôm nay theo định dạng YYYY-MM-DD
-//
-//     if ($("#fullName").val().trim() === '') {
-//         $("#errorFullName").html('Please enter full name');
-//         count++;
-//     }
-//
-//     // Kiểm tra Date of Birth (DOB)
-//     var dob = $("#dob").val();
-//     if (dob.trim() === '') {
-//         $("#errorDob").html('Please enter date of birth');
-//         count++;
-//     } else if (dob >= today) {
-//         $("#errorDob").html('Date of birth must be in the past');
-//         count++;
-//     }
-//     //Kiểm tra Phone
-//     var phone = $("#phone").val().trim();
-//     if (phone === '') {
-//         $("#errorPhone").html('Please enter phone number');
-//         count++;
-//     } else if (!phoneRegex.test(phone)) {
-//         $("#errorPhone").html('Invalid phone number format');
-//         count++;
-//     }
-//     if ($('#role :selected').val() === '') {
-//         $("#errorRole").html('Please select a role');
-//         count++;
-//     }
-//
-//     if (count === 0) {
-//         var user = {
-//             id: $("#userID").val(),
-//             fullName: $("#fullName").val().trim(),
-//             email: $("#email").val().trim(),
-//             dob: $("#dob").val().trim(),
-//             phone: $("#phone").val().trim(),
-//             roleId: $('#role :selected').val(),
-//             status: $("#status").val().trim()
-//         };
-//
-//         console.log("User ID: ", $("#userID").val()); // Kiểm tra userID
-//         console.log("URL: ", "/admin/api/edit-user/" + $("#userID").val()); // Kiểm tra URL
-//         console.log(JSON.stringify(user))
-//
-//         $.ajax({
-//             url: "/admin/api/edit-user",
-//             type: "POST",
-//             contentType: "application/json",
-//             data: JSON.stringify(user),
-//             success: function(responseData) {
-//                 alert(responseData.message);
-//             },
-//             error: function(responseData) {
-//                 if (responseData.responseJSON) {
-//                     $("#errorFullName").html(responseData.responseJSON.fullName);
-//                     $("#errorEmail").html(responseData.responseJSON.email);
-//                     $("#errorDob").html(responseData.responseJSON.dob);
-//                     $("#errorPhone").html(responseData.responseJSON.phone);
-//                     $("#errorRole").html(responseData.responseJSON.roleId);
-//                     $("#errorStatus").html(responseData.responseJSON.status);
-//                 }
-//             }
-//         });
-//     }
-// });
+document.getElementById("cancel-button").addEventListener("click", function () {
+    var previousUrl = localStorage.getItem('previousUrl');
+    if (previousUrl) {
+        window.location.href = previousUrl;
+        localStorage.removeItem('previousUrl');
+    } else {
+        window.history.back();
+    }
+});
+
