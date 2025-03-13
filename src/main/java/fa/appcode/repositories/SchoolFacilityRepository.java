672aa8f018ac8ba0f3f7dbff374f888cc2fe6abf
Haivd7 <haivd7@fpt.com>
2025-03-13 09:25:54 +0700

    [Module-06][Haivd7]: Fix bug for function My School

--- src/main/java/fa/appcode/repositories/SchoolFacilityRepository.java
+++ src/main/java/fa/appcode/repositories/SchoolFacilityRepository.java
@@ -3,6 +3,10 @@ package fa.appcode.repositories;
 import fa.appcode.entities.SchoolFacility;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SchoolFacilityRepository extends JpaRepository<SchoolFacility, Long> {
 
+
+    List<SchoolFacility> getSchoolFacilitiesBySchool_Id(Integer schoolId);
 }
