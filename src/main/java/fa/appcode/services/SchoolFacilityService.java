bf851d2275d2736e45573237da5bddd9fc1f2a13
anhdq43 <anhdq43@fpt.com>
2025-03-21 16:20:10 +0700

    [module-03][anhdq43] update uc update school

--- src/main/java/fa/appcode/services/SchoolFacilityService.java
+++ src/main/java/fa/appcode/services/SchoolFacilityService.java
@@ -1,7 +1,12 @@
 package fa.appcode.services;
 
+import fa.appcode.entities.SchoolFacility;
+import fa.appcode.entities.SchoolInfo;
+import org.springframework.dao.DataAccessException;
+
 import java.util.List;
 
 public interface SchoolFacilityService {
     List<Integer> getAllSchoolFacilityIdBySchoolIdAndNoDeleteFlg(int schoolId);
+    int saveAllSchoolFacility(List<Integer> schoolUtilityId, SchoolInfo schoolInfo) throws DataAccessException;
 }
--- src/main/java/fa/appcode/services/impl/SchoolUtilityServiceImpl.java
+++ src/main/java/fa/appcode/services/impl/SchoolUtilityServiceImpl.java
@@ -1,11 +1,18 @@
 package fa.appcode.services.impl;
 
+import fa.appcode.common.utils.RoleConstant;
+import fa.appcode.entities.SchoolInfo;
+import fa.appcode.entities.SchoolUtility;
+import fa.appcode.entities.SchoolUtilityId;
 import fa.appcode.repositories.SchoolUtilityRepository;
 import fa.appcode.services.SchoolUtilityService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +22,24 @@ public class SchoolUtilityServiceImpl implements SchoolUtilityService {
     @Override
     public List<Integer> getAllSchoolUtilityIdBySchoolIdAndNoDelete(int schoolId) {
         return schoolUtilityRepository.getAllSchoolUtilityIdBySchoolId(schoolId, false);
+    }
+
+    @Override
+    public int saveAllSchoolUtility(List<Integer> schoolUtilityId, SchoolInfo schoolInfo) throws DataAccessException {
+        return schoolUtilityRepository.saveAll(schoolUtilityId.stream()
+                .map(utilityId -> {
+                    SchoolUtility su = new SchoolUtility();
+                    su.setId(new SchoolUtilityId(schoolInfo.getId(), utilityId));
+                    su.setSchool(schoolInfo);
+                    su.setCreateId(RoleConstant.SCHOOL_OWNER);
+                    su.setCreateTime(Instant.now());
+                    su.setUpdateId(RoleConstant.SCHOOL_OWNER);
+                    su.setUpdateTime(Instant.now());
+                    su.setDeleteFlg(false);
+                    su.setRecordNo(1);
+                    return su;
+                })
+                .collect(Collectors.toList())
+        ).size();
     }
 }
